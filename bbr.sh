cbe111714c8342e8bb59c65311e7fbecbf1c81b6
Hadi SamimiFar <62488543+samimifar@users.noreply.github.com>
2024-04-05 23:50:19 +0330

    :)
    :)

---
+++ bbr.sh
@@ -0,0 +1,56 @@
+red='\033[0;31m'
+green='\033[0;32m'
+yellow='\033[0;33m'
+plain='\033[0m'
+
+# Check OS and set release variable
+if [[ -f /etc/os-release ]]; then
+    source /etc/os-release
+    release=$ID
+elif [[ -f /usr/lib/os-release ]]; then
+    source /usr/lib/os-release
+    release=$ID
+else
+    echo "Failed to check the system OS, please contact the author!" >&2
+    exit 1
+fi
+echo "The OS release is: ${green}$release${plain}"
+
+if grep -q "net.core.default_qdisc=fq" /etc/sysctl.conf && grep -q "net.ipv4.tcp_congestion_control=bbr" /etc/sysctl.conf; then
+        echo -e "${yellow}BBR${plain} is already ${green}enabled!${plain}"
+        exit 0
+fi
+
+# Check the OS and install necessary packages
+case "${release}" in
+    ubuntu | debian | armbian)
+        apt-get update && apt-get install -yqq --no-install-recommends ca-certificates > /dev/null 2>&1 &
+        ;;
+    centos | almalinux | rocky | oracle)
+        yum -y update && yum -y install ca-certificates > /dev/null 2>&1 &
+        ;;
+    fedora)
+        dnf -y update && dnf -y install ca-certificates > /dev/null 2>&1 &
+        ;;
+    arch | manjaro)
+        pacman -Sy --noconfirm ca-certificates > /dev/null 2>&1 &
+        ;;
+    *)
+        echo -e "${red}Unsupported operating system. Please check the script and install the necessary packages manually.${plain}\n"
+        exit 1
+        ;;
+    esac
+    wait $!
+    # Enable BBR
+    echo "net.core.default_qdisc=fq" | tee -a /etc/sysctl.conf
+    echo "net.ipv4.tcp_congestion_control=bbr" | tee -a /etc/sysctl.conf
+
+    # Apply changes
+    sysctl -p
+    wait $!
+    # Verify that BBR is enabled
+    if [[ $(sysctl net.ipv4.tcp_congestion_control | awk '{print $3}') == "bbr" ]]; then
+        echo -e "${green}BBR has been enabled successfully.${plain}"
+    else
+        echo -e "${red}Failed to enable BBR. Please check your system configuration.${plain}"
+    fi
